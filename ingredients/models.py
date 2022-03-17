5719024ef3914ef100978d991a1c6f85690c6320
wojtasemzi <wojciech.wantuch@gmail.com>
2022-03-17 12:36:08 +0100

    add Ingredient model

--- ingredients/models.py
+++ ingredients/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    description = models.TextField()
+    energy_in_kcal = models.IntegerField(blank=False)
+    fat = models.FloatField(blank=False)
+    saturated_fat = models.FloatField(blank=False)
+    carbohydrate = models.FloatField(blank=False)
+    sugars = models.FloatField(blank=False)
+    fibre = models.FloatField(blank=False)
+    protein = models.FloatField(blank=False)
+    salt = models.FloatField(blank=False)
+
+    #TODO: vitamins Ingredient_Vitamin
+    #TODO: minerals Ingredient_Mineral
+    #TODO: tags Ingredient_Tag
