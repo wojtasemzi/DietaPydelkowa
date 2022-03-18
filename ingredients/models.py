d1fc979f4de4f768fac6400b8af30f2331f5d63c
wojtasemzi <wojciech.wantuch@gmail.com>
2022-03-18 07:57:11 +0100

    add many to many rel Recipe_Ingredient

--- ingredients/models.py
+++ ingredients/models.py
@@ -1,4 +1,3 @@
-from tkinter import CASCADE
 from django.db import models
 
 class Ingredient(models.Model):
@@ -61,12 +60,12 @@ class Recipe(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     recipe = models.TextField()
-    #TODO: ingredients = Recipe_Ingredient
+    ingredients = models.ManyToManyField(through='Recipe_Ingredient', to=Ingredient)
     preparation_time = models.IntegerField()
 
 
 class Recipe_Ingredient(models.Model):
-    recipe_id = models.ForeignKey(Recipe, on_delete=CASCADE)
-    ingredient_id = models.ForeignKey(Ingredient, on_delete=CASCADE)
+    recipe_id = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient_id = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
     quantity = models.FloatField()
     unit_id = models.ForeignKey(Unit, on_delete=models.RESTRICT)
