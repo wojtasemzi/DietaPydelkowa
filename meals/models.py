fb089ed2520555af419c8ff994a8b1dc25f5e9b0
wojtasemzi <wojciech.wantuch@gmail.com>
2022-03-18 08:55:43 +0100

    add Meal model

--- meals/models.py
+++ meals/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from ingredients.models import Tag
+from recipes.models import Recipe
+
+class Meal(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    recipes = models.ManyToManyField(Recipe)
+    tags = models.ManyToManyField(Tag)
