05e60e6532cdda6f19c606bdb1e69747e4aecb27
wojtasemzi <wojciech.wantuch@gmail.com>
2022-03-19 07:42:53 +0100

    add many to many rel Plan_Tag

--- plans/models.py
+++ plans/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
 from meals.models import Meal
+from ingredients.models import Tag
 
 
 class Plan(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     meals = models.ManyToManyField(through='Plan_Meal', to=Meal)
+    tags = models.ManyToManyField(Tag)
 
 
 class Plan_Meal(models.Model):
