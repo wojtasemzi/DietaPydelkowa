a61863ee1c9eff2c64b46009adf7b57a0598f311
wojtasemzi <wojciech.wantuch@gmail.com>
2022-03-19 07:36:54 +0100

    add Plan_meal model

--- plans/models.py
+++ plans/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 
+from meals.models import Meal
+
 
 class Plan(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     #TODO: meals = models.ManyToManyField(through='Plan_Meal', to=Meal)
+
+
+class Plan_Meal(models.Model):
+    plan_id = models.ForeignKey(Plan)
+    meal_id = models.ForeignKey(Meal)
+    #TODO: day = models.ForeignKey('Day')
+    order = models.IntegerField()
