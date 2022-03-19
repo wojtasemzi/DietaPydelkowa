05b5142fd27eda23d9ebc7d0d05c36aac616c3d2
wojtasemzi <wojciech.wantuch@gmail.com>
2022-03-19 07:31:06 +0100

    add Plan model

--- plans/models.py
+++ plans/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Plan(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    #TODO: meals = models.ManyToManyField(through='Plan_Meal', to=Meal)
