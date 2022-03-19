9017740fee4a7079bdb1402fe8ae5136ac800f2d
wojtasemzi <wojciech.wantuch@gmail.com>
2022-03-19 07:37:47 +0100

    add many to many rel Plan_Meal

--- plans/models.py
+++ plans/models.py
@@ -6,7 +6,7 @@ from meals.models import Meal
 class Plan(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
-    #TODO: meals = models.ManyToManyField(through='Plan_Meal', to=Meal)
+    meals = models.ManyToManyField(through='Plan_Meal', to=Meal)
 
 
 class Plan_Meal(models.Model):
