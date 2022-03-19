66fd50fc751a96425ce5bf78657d251b782cd0da
wojtasemzi <wojciech.wantuch@gmail.com>
2022-03-19 12:32:07 +0100

    move prices code to price app

--- ingredients/models.py
+++ ingredients/models.py
@@ -30,27 +30,3 @@ class Mineral(models.Model):
 class Tag(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(blank=True)
-
-
-class Price(models.Model):
-    ingredient_id = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
-    price = models.FloatField()
-    currency_id = models.ForeignKey('Currency', on_delete=models.RESTRICT)
-    quantity = models.FloatField()
-    unit_id = models.ForeignKey('Unit', on_delete=models.RESTRICT)
-    store_id = models.ForeignKey('Store', on_delete=models.RESTRICT)
-
-
-class Currency(models.Model):
-    currency = models.CharField(max_length=50)
-    iso_4217 = models.CharField(max_length=3)
-
-
-class Unit(models.Model):
-    unit = models.CharField(max_length=20)
-    abbreviation = models.CharField(max_length=5)
-
-
-class Store(models.Model):
-    store = models.CharField(max_length=50)
-    address = models.CharField(max_length=100)
--- ingredients/urls.py
+++ ingredients/urls.py
@@ -35,36 +35,4 @@ urlpatterns = [
     path('tag/<int:pk>/edit/', views.TagEdit.as_view(), name='tag_edit'),
     path('tag/<int:pk>/delete/', views.TagDelete.as_view(), name='tag_delete'),
     # endregion
-    # region Price
-    path('price/', views.Prices.as_view(), name='prices'),
-
-    path('price/add/', views.PriceAdd.as_view(), name='price_add'),
-    path('price/<int:pk>/', views.Price.as_view(), name='price'),
-    path('price/<int:pk>/edit/', views.PriceEdit.as_view(), name='price_edit'),
-    path('price/<int:pk>/delete/', views.PriceDelete.as_view(), name='price_delete'),
-    # endregion
-    # region Currency
-    path('currency/', views.Currencys.as_view(), name='currencys'),
-
-    path('currency/add/', views.CurrencyAdd.as_view(), name='currency_add'),
-    path('currency/<int:pk>/', views.Currency.as_view(), name='currency'),
-    path('currency/<int:pk>/edit/', views.CurrencyEdit.as_view(), name='currency_edit'),
-    path('currency/<int:pk>/delete/', views.CurrencyDelete.as_view(), name='currency_delete'),
-    # endregion
-    # region Unit
-    path('unit/', views.Units.as_view(), name='units'),
-
-    path('unit/add/', views.UnitAdd.as_view(), name='unit_add'),
-    path('unit/<int:pk>/', views.Unit.as_view(), name='unit'),
-    path('unit/<int:pk>/edit/', views.UnitEdit.as_view(), name='unit_edit'),
-    path('unit/<int:pk>/delete/', views.UnitDelete.as_view(), name='unit_delete'),
-    # endregion
-    # region Store
-    path('store/', views.Stores.as_view(), name='stores'),
-
-    path('store/add/', views.StoreAdd.as_view(), name='store_add'),
-    path('store/<int:pk>/', views.Store.as_view(), name='store'),
-    path('store/<int:pk>/edit/', views.StoreEdit.as_view(), name='store_edit'),
-    path('store/<int:pk>/delete/', views.StoreDelete.as_view(), name='store_delete'),
-    # endregion
 ]
